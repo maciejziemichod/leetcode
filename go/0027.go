4f46c477c74d356f1cbf82d2bc8255c06a8fdecc
maciejziemichod <maciej.ziemichod@gmail.com>
2024-04-18 20:43:23 +0200

    feat: 27

---
+++ go/0027.go
@@ -0,0 +1,29 @@
+package main
+
+// https://leetcode.com/problems/remove-element/description/https://leetcode.com/problems/remove-element/description/
+func removeElementV1(nums []int, val int) int {
+	r := 0
+	for _, num := range nums {
+		if num != val {
+			nums[r] = num
+			r++
+		}
+	}
+
+	return r
+}
+
+func removeElementV2(nums []int, val int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		if nums[left] == val {
+			nums[left], nums[right] = nums[right], nums[left]
+			right--
+		} else {
+			left++
+		}
+	}
+
+	return left
+}
